1bf47bd437cad483211c24ce701f0f326b0f887c
xiaoyutianL <128241333+svip-leipeng@users.noreply.github.com>
2023-12-16 11:18:09 +0800

    Update WangzheSkin.py
    显示所有英雄的名字。用户可以通过单选按钮选择要下载的英雄，点击“选择保存文件夹”按钮选择保存皮肤图片的文件夹，最后点击“开始下载”按钮开始下载。

--- WangzheSkin.py
+++ WangzheSkin.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import time
+import random
+from tkinter import *
+from tkinter import filedialog
 
 url = 'https://pvp.qq.com/web201605/js/herolist.json'
 herolist = requests.get(url)
@@ -21,4 +24,24 @@ def downloadPic():
                 open(str(k) + '.jpg', 'wb').write(im.content)
             time.sleep(random.randint(10, 20))
 
-downloadPic()
+def select_folder():
+    folder_path = filedialog.askdirectory()
+    return folder_path
+
+root = Tk()
+root.title("英雄皮肤下载器")
+
+label = Label(root, text="请选择要下载的英雄：")
+label.pack()
+
+for i, name in enumerate(hero_name):
+    button = Radiobutton(root, text=name, variable=StringVar(), value=i)
+    button.pack()
+
+folder_button = Button(root, text="选择保存文件夹", command=select_folder)
+folder_button.pack()
+
+start_button = Button(root, text="开始下载", command=downloadPic)
+start_button.pack()
+
+root.mainloop()
