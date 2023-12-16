0e521fb8184c609fdb2f9ff7e85a07c51aafebc0
xiaoyutianL <128241333+svip-leipeng@users.noreply.github.com>
2023-12-16 11:47:15 +0800

    Update WangzheSkin.py
    加入浏览器代理，每下载完一个英雄更换一个浏览器代理头

--- WangzheSkin.py
+++ WangzheSkin.py
@@ -2,8 +2,7 @@ import os
 import requests
 import time
 import random
-from tkinter import *
-from tkinter import filedialog
+from fake_useragent import UserAgent
 
 url = 'https://pvp.qq.com/web201605/js/herolist.json'
 herolist = requests.get(url)
@@ -19,29 +18,11 @@ def downloadPic():
         i += 1
         for k in range(10):
             onehero_link = 'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/' + str(j) + '/' + str(j) + '-bigskin-' + str(k) + '.jpg'
-            im = requests.get(onehero_link)
+            ua = UserAgent()
+            headers = {'User-Agent': ua.random}
+            im = requests.get(onehero_link, headers=headers)
             if im.status_code == 200:
                 open(str(k) + '.jpg', 'wb').write(im.content)
             time.sleep(random.randint(10, 20))
 
-def select_folder():
-    folder_path = filedialog.askdirectory()
-    return folder_path
-
-root = Tk()
-root.title("英雄皮肤下载器")
-
-label = Label(root, text="请选择要下载的英雄：")
-label.pack()
-
-for i, name in enumerate(hero_name):
-    button = Radiobutton(root, text=name, variable=StringVar(), value=i)
-    button.pack()
-
-folder_button = Button(root, text="选择保存文件夹", command=select_folder)
-folder_button.pack()
-
-start_button = Button(root, text="开始下载", command=downloadPic)
-start_button.pack()
-
-root.mainloop()
+downloadPic()
