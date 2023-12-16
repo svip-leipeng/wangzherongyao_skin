a6ab7fccee6fb7c82d28e85c1fa314c1e8fbf80a
xiaoyutianL <128241333+svip-leipeng@users.noreply.github.com>
2023-12-16 11:04:21 +0800

    Update WangzheSkin.py
    1.在每一步操作后，使用time.sleep()函数暂停一段时间，随机休眠10-20秒。

--- WangzheSkin.py
+++ WangzheSkin.py
@@ -1,30 +1,24 @@
 import os
 import requests
+import time
 
 url = 'https://pvp.qq.com/web201605/js/herolist.json'
-herolist = requests.get(url)  # 获取英雄列表json文件
+herolist = requests.get(url)
+herolist_json = herolist.json()
+hero_name = list(map(lambda x: x['cname'], herolist_json))
+hero_number = list(map(lambda x: x['ename'], herolist_json))
 
-herolist_json = herolist.json()  # 转化为json格式
-hero_name = list(map(lambda x: x['cname'], herolist.json()))  # 提取英雄的名字
-hero_number = list(map(lambda x: x['ename'], herolist.json()))  # 提取英雄的编号
-
-
-# 下载图片
 def downloadPic():
     i = 0
     for j in hero_number:
-        # 创建文件夹
-        os.makedirs("C:\\Users\\XXXXX\\XXXXX\\skin\\" + hero_name[i])#原因是因为os.listdir()只能创建一级目录。而os.makedirs()可以创建多级目录。
-        # 进入创建好的文件夹
+        os.makedirs("C:\\Users\\XXXXX\\XXXX\\skin\\" + hero_name[i])
         os.chdir("C:\\Users\\XXXXX\\XXXX\\skin\\" + hero_name[i])
         i += 1
         for k in range(10):
-            # 拼接url
-            onehero_link = 'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/' + str(j) + '/' + str(
-                j) + '-bigskin-' + str(k) + '.jpg'
-            im = requests.get(onehero_link)  # 请求url
+            onehero_link = 'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/' + str(j) + '/' + str(j) + '-bigskin-' + str(k) + '.jpg'
+            im = requests.get(onehero_link)
             if im.status_code == 200:
-                open(str(k) + '.jpg', 'wb').write(im.content)  # 写入文件
-
+                open(str(k) + '.jpg', 'wb').write(im.content)
+            time.sleep(random.randint(10, 20))
 
 downloadPic()
